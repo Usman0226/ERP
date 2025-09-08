dd9952f8a00d2aefa056dea845b5a45aced0e6b8
tchaithanya377 <t.chaitanya377@gmail.com>
2025-09-08 17:45:20 +0530

    v0.1.3

--- campshub360/settings.py
+++ campshub360/settings.py
@@ -43,7 +43,7 @@ DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv(
     'ALLOWED_HOSTS',
-    'localhost,127.0.0.1,testserver,campushub360.xyz,www.campushub360.xyz,api.campushub360.xyz,.campushub360.xyz,3.109.5.217,ec2-3-109-5-217.ap-south-1.compute.amazonaws.com,13.201.129.254,ec2-13-201-129-254.ap-south-1.compute.amazonaws.com'
+    'localhost,127.0.0.1,testserver,campushub360.xyz,www.campushub360.xyz,api.campushub360.xyz,.campushub360.xyz,13.232.220.214,ec2-13.232.220.214ap-south-1.compute.amazonaws.com,13.232.220.214,ec2-13.232.220.214.ap-south-1.compute.amazonaws.com'
 ).split(',')
 
 
@@ -359,7 +359,7 @@ CORS_ALLOWED_ORIGINS = os.getenv(
     'http://localhost:3000,http://127.0.0.1:3000,http://0.0.0.0:3000,https://localhost:3000,https://127.0.0.1:3000,'
     'http://localhost:5173,http://127.0.0.1:5173,http://0.0.0.0:5173,https://localhost:5173,https://127.0.0.1:5173,'
     'https://campushub360.xyz,https://www.campushub360.xyz,https://api.campushub360.xyz,'
-    'http://3.109.5.217,https://3.109.5.217,http://ec2-3-109-5-217.ap-south-1.compute.amazonaws.com,https://ec2-3-109-5-217.ap-south-1.compute.amazonaws.com,'
+    'http://13.232.220.214,https://13.232.220.214,http://ec2-13.232.220.214.ap-south-1.compute.amazonaws.com,https://ec2-13.232.220.214.ap-south-1.compute.amazonaws.com,'
     'http://13.201.129.254,https://13.201.129.254,http://ec2-13-201-129-254.ap-south-1.compute.amazonaws.com,https://ec2-13-201-129-254.ap-south-1.compute.amazonaws.com'
 ).split(',')
 CORS_ALLOW_CREDENTIALS = True
@@ -371,7 +371,7 @@ CSRF_TRUSTED_ORIGINS = os.getenv(
     'http://localhost:5173,http://127.0.0.1:5173,http://0.0.0.0:5173,https://localhost:5173,https://127.0.0.1:5173,'
     'http://127.0.0.1:8000,http://localhost:8000,https://localhost:8000,'
     'https://campushub360.xyz,https://www.campushub360.xyz,https://api.campushub360.xyz,'
-    'http://3.109.5.217,https://3.109.5.217,http://ec2-3-109-5-217.ap-south-1.compute.amazonaws.com,https://ec2-3-109-5-217.ap-south-1.compute.amazonaws.com,'
+    'http://13.232.220.214,https://13.232.220.214,http://ec2-13.232.220.214.ap-south-1.compute.amazonaws.com,https://ec2-13.232.220.214.ap-south-1.compute.amazonaws.com,'
     'http://13.201.129.254,https://13.201.129.254,http://ec2-13-201-129-254.ap-south-1.compute.amazonaws.com,https://ec2-13-201-129-254.ap-south-1.compute.amazonaws.com'
 ).split(',')
 
